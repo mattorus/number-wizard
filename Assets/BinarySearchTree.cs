a4559046636e577dd51986fdeec5052d83180193
mattorus <mattorus@gmail.com>
2019-04-10 13:39:34 -0700

    Implement BST for guessing logic

---
+++ Assets/BinarySearchTree.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets
+{
+    class BinarySearchTree
+    {
+        TreeNode Root { get; set; }
+
+        public BinarySearchTree(TreeNode root)
+        {
+            Root = root;
+        }
+
+        public static TreeNode GenerateRandomTree(int min, int max)
+        {
+            Random rand = new Random();
+            int root = rand.Next(min, max + 1);
+            TreeNode treeRoot = new TreeNode(root);
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i=min; i <= max; i++)
+            {
+                map.Add(i, i);
+            }
+
+            treeRoot = new TreeNode(root);
+            map.Remove(root);
+
+            while (map.Count > 0)
+            {
+                int randNum = rand.Next(min, max + 1);
+                if (map.Remove(randNum))
+                {
+                    treeRoot.Add(randNum);
+                    Console.WriteLine($"Adding {randNum}...");
+                }
+            }
+
+
+            return treeRoot;
+        }
+
+    }
+
+    class TreeNode
+    {
+        public int Val { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int val, TreeNode left=null, TreeNode right=null)
+        {
+            Val = val;
+            Left = left;
+            Right = right;
+        }
+
+        public void Add(int add)
+        {
+            TreeNode newNode;
+            if (add > Val)
+            {
+                if (Right == null || Right.Val > add)
+                {
+                    newNode = new TreeNode(add, null, Right);
+                    Right = newNode;
+                }
+                else
+                {
+                    Right.Add(add);
+                }
+            }
+            else
+            {
+                if (Left == null || Left.Val < add)
+                {
+                    newNode = new TreeNode(add, Left, null);
+                    Left = newNode;
+                }
+                else
+                {
+                    Left.Add(add);
+                }
+            }
+        }
+    }
+}
--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets;
 
 public class NumberWizard : MonoBehaviour
 {   
     private static int DefaultMin = 1;
     private static int DefaultMax = 1000;
     private static int DefaultGuess = DefaultMax / 2;
-
+    private TreeNode tree;
+    private System.Random rand;
     // Game 
     private int Min;
     private int Max;
@@ -24,7 +26,7 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow) && !IsGameOver)
         {
-            Min = Guess;
+            Min = Guess + 1;
             UpdateGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow) && !IsGameOver)
@@ -50,7 +52,9 @@ public class NumberWizard : MonoBehaviour
 
     private void UpdateGuess()
     {
-        Guess = (Min + Max) / 2;
+        //Guess = (Min + Max) / 2;
+        Guess = rand.Next(Min, Max);
+        tree.Add(Guess);
         Debug.Log($"Is your number {Guess}?");
         ShowControls();
     }
@@ -59,8 +63,11 @@ public class NumberWizard : MonoBehaviour
     {   
         Min = NumberWizard.DefaultMin;
         Max = NumberWizard.DefaultMax;
-        Guess = NumberWizard.DefaultGuess;
+        //Guess = NumberWizard.DefaultGuess;
         IsGameOver = false;
+        rand = new System.Random();
+        Guess = rand.Next(Min, Max + 1);
+        tree = new TreeNode(Guess);
 
         Debug.Log("Greetings, friend! Welcome to Number Wizard!");
         Debug.Log("Pick a number...keep it secret, keep it safe!");
@@ -72,6 +79,22 @@ public class NumberWizard : MonoBehaviour
     private void Victory()
     {
         Debug.Log($"Victory!");
+        Debug.Log($"Here are all my guesses:\n\t");
+        int guessCount = 0;
+        while (tree != null)
+        {
+            guessCount++;
+            Debug.Log($"{tree.Val}\n\t");
+            if (tree.Right != null)
+            {
+                tree = tree.Right;
+            }
+            else
+            {
+                tree = tree.Left;
+            }
+        }
+        Debug.Log($"\nIt only took me {guessCount} tries! :D\n\n");
         Debug.Log($"Play Again?\n\tS = Start Over, Q = Quit");
         IsGameOver = true;
     }
